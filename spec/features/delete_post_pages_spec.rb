e291ca2d87e91b08acb576144a7a9cccfff089c9
jocelyn alsdorf & jeanette fairless <student@epicodus.com>
2015-07-01 10:48:57 -0700

    passing spec for delete post functionality/path

---
+++ spec/features/delete_post_pages_spec.rb
@@ -0,0 +1,10 @@
+require 'rails_helper'
+
+describe 'the delete post process' do
+  it 'deletes a post' do
+    test_post = Post.create(:title => 'Spicy Cheese Burger', :content => 'yum', :author => 'Paul')
+    visit post_path(test_post)
+    click_on 'Delete'
+    expect(page).to have_no_content 'Spicy Cheese Burger'
+  end
+end
