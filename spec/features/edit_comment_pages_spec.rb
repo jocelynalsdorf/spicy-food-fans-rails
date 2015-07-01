ded796557bb277cafe17b82b03d63ca1a56a84c5
jocelyn alsdorf & jeanette fairless <student@epicodus.com>
2015-07-01 12:49:47 -0700

    passing specs for errors for edit comment page

--- spec/features/edit_comment_pages_spec.rb
+++ spec/features/edit_comment_pages_spec.rb
@@ -13,12 +13,13 @@ describe "the edit comment process" do
     expect(page).to have_content 'Rat'
   end
 
-  # it "gives an error when no data is entered into form fields" do
-  #   test_section = Section.create(:title => "Java")
-  #   test_lesson = Lesson.create(:name => "ruby", :content => "blah", :number => 2)
-  #   visit edit_section_lesson_path(test_section, test_lesson)
-  #   fill_in 'Name', :with => ''
-  #   click_on 'Update Lesson'
-  #   expect(page).to have_content 'errors'
-  # end
+  it "gives an error when no data is entered into form fields" do
+    test_post = Post.create(:title => "Java", :content => "blah blah", :author => 'me', id: 2)
+    test_comment = Comment.create(:text => "text 1", :rating => 1, :user => 'me', :post_id => test_post.id)
+    visit post_path(test_post, test_comment)
+    find('li', :text => 'text 1').click_link('Edit Comment')
+    fill_in 'Text', :with => ''
+    click_on 'Update Comment'
+    expect(page).to have_content 'errors'
+  end
 end
